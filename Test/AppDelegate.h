1fe34631ed657945b46e5ce7c46bd664edd142ce
FYZ <1790462158@qq.com>
2017-06-15 20:44:39 +0800

    xxx

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 麦子 on 2017/6/15.
+//  Copyright © 2017年 com.fujia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
